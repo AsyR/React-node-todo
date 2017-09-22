2638cc6faa93243b8ebc50f44dfa078909b3b1d3
Michael Thane <Asy-dk@live.dk>
2017-09-22 21:57:55 +0200

    delete task
    added functionality for deleting a task

--- src/index.js
+++ src/index.js
@@ -9,36 +9,37 @@ class ToDoBox extends React.Component{
   constructor(){
     super();
     this.state = {
-      tasks: [{id: 1, author: 'Michael Thane', body:'great picture'},
-      {id: 2, author: 'hest', body:' excellent stuff!'}]
+      tasks: [{id: 1, author: 'RedX Tech', body:'great picture', key: 1},
+      {id: 2, author: 'hest', body:' excellent stuff!', key: 2}]
     }
+    this.deleteTask = this.deleteTask.bind(this);
 
   }
   _getTasks() {
     return this.state.tasks.map((task) => {
       return (
       <ToDoTask
-        body={task.body} />
+        body={task.body}
+        handleDelete={this.deleteTask}
+        key={task.key}
+        id={task.id}/>
         );
       });
-
-    // const taskList = [
-    //   ];
-    //   return taskList.map((task) => {
-    //     return (
-    //       <ToDoTask author ={task.author} body={task.body} key={task.id} />
-    //     );
   }
+
+
   render() {
+
     const tasks = this._getTasks();
     return(
-      <div className='ToDoBox col-xs-6 col-xs-offset-3 borderradius'>
-        <div className='row' id='ToDoBox-header-row'>
-          <div className="col-xs-12">
-            <h1 className='title center'> To Do Application </h1>
+      <div className='uk-width-1 uk-flex-center uk-flex'>
+      <div className='ToDoBox uk-grid borderradius uk-width-1-3 '>
+        <div id='ToDoBox-header-row' className='uk-width-1-1@s uk-flex-center uk-flex '>
+          <div className=" ">
+            <h1 className='title'> To Do Application </h1>
             </div>
           </div>
-          <div className='row' id='ToDoBox-form-row'>
+          <div className='' id='ToDoBox-form-row'>
             <ToDoForm addToDoTask={this._addToDoTask.bind(this)} />
             </div>
           <div className='row' id='ToDoBox-tasks-row'>
@@ -48,24 +49,38 @@ class ToDoBox extends React.Component{
         </div>
         <div className='row' id ='ToDoBox-footer-row'></div>
       </div>
+      </div>
     );
   }
   _addToDoTask(body){
     const todotask = {
       id: this.state.tasks.length +1,
+      key: this.state.tasks.length +1,
       body
+
     };
     this.setState({ tasks: this.state.tasks.concat([todotask])});
   }
+
+  deleteTask (task) {
+    let tempStateTasks = []
+    this.state.tasks.forEach((taskItem) => {
+      if (taskItem.id !== task) {
+        tempStateTasks.push(taskItem)
+      }
+    })
+    this.setState({
+      tasks: tempStateTasks
+    })
+  }
 }
-// ToDoTask author='Michael Thane' body='Jeg skal til aftensmad i aften' />
 
 class ToDoForm extends React.Component{
   render(){
     return(
       <form className="task-form height" onSubmit={this._taskSubmit.bind(this)}>
-        <div className='task-form-fields height font-size'>
-          <input className ='col-xs-8 col-xs-offset-2 height borderradius' placeholder='Task:' ref={(input) => this._task_body = input} />
+        <div className='task-form-fields height font-size uk-grid uk-width-2-3'>
+          <input className =' uk-width-1 uk-flex uk-flex-middle height borderradius' placeholder='Task:' ref={(input) => this._task_body = input} />
         </div>
         <button className='uk-button uk-button-primary'> Submit</button>
       </form>
@@ -83,11 +98,10 @@ class ToDoTask extends React.Component {
     return(
       <div className ='task'>
         <hr />
-        <button className='uk-button-small uk-button-danger'>&#x2716;</button>
+        <button className='uk-button-small uk-button-danger' onClick={() => {this.props.handleDelete(this.props.id)}}>&#x2716;</button>
         <p className='comment-header'> {this.props.author}</p>
         <p className='comment-body'>{this.props.body}</p>
         <p className='comment-footer'> comment footer text</p>
-
       </div>
 
     );
