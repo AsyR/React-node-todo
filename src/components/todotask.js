ee76aa2b801fc771bae6fbba046900425ab37a3a
Michael Thane <Asy-dk@live.dk>
2017-10-09 03:21:22 +0200

    update
    changes class on checkbox click. Checkbox now toggles

--- src/components/todotask.js
+++ src/components/todotask.js
@@ -2,6 +2,7 @@ import React from 'react';
 export default class ToDoTask extends React.Component {
   constructor(){
   super()
+
   this.handleCheckBox = this.handleCheckBox.bind(this)
   this.state = {date: new Date(),
   completed: false};
@@ -11,7 +12,7 @@ export default class ToDoTask extends React.Component {
       <div className={this.state.completed ? "completed" : "" } id='single-task'>
         <hr />
           <button className='button hollow alert right' onClick={() => {this.props.handleDelete(this.props.id)}}>&#x2716;</button>
-          <input type="checkbox" className='toggle' onChange={this.handleCheckBox} checked={this.state.completed}/>
+          <input type="checkbox" className='toggle' onChange={this.handleCheckBox} checked={this.state.isChecked}/>
           <p className='comment-header'> {this.props.author}</p>
           <p className='comment-body'>{this.props.body}</p>
           <p className='comment-footer'>{this.state.date.toLocaleTimeString()}</p>
@@ -20,5 +21,6 @@ export default class ToDoTask extends React.Component {
   }
   handleCheckBox(e) {
   this.props.completeTask(this.props.id);
+  this.state.completed = true;
   }
 }
